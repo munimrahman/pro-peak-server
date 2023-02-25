76425f47f6484263587c1a8eef19d649d7a4ad5e
munimrahman <munimrh@gmail.com>
2023-02-25 21:40:39 +0600

    config

---
+++ config/index.js
@@ -0,0 +1,10 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const config = {
+  PORT: process.env.PORT || 5005,
+  DATABASE_URL: process.env.DB_URL || "",
+};
+
+module.export = config;
